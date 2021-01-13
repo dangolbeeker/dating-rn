2a7d127d242a5e51613f3fd67499c8bff505685c
dangolbeeker <magiver273@gmail.com>
2021-01-13 00:41:37 -0500

    supergrid example import/breaking other stuff

--- datingapp/app/store/actions/types.ts
+++ datingapp/app/store/actions/types.ts
@@ -2,6 +2,7 @@
 export const LOGIN_ENABLE_LOADER = 'LOGIN_ENABLE_LOADER';
 export const LOGIN_DISABLE_LOADER = 'LOGIN_DISABLE_LOADER';
 
+
 // theming
 export const TOGGLE_THEME = 'TOGGLE_THEME';
 
@@ -11,3 +12,22 @@ export const LOGIN_RESPONSE = 'LOGIN_RESPONSE';
 export const LOGIN_LOADING_ENDED = 'LOGIN_LOADING_ENDED';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 export const LOG_OUT = 'LOG_OUT';
+
+// feed
+// export const FEED_
+// export const FEED_
+// export const FEED_  
+
+
+
+
+
+
+// streaming
+// export const STREAM_
+// export const STREAM_
+// export const STREAM_
+// export const STREAM_
+
+
+
--- datingapp/app/store/reducers/index.ts
+++ datingapp/app/store/reducers/index.ts
@@ -4,4 +4,8 @@
 import * as loadingReducer from './loadingReducer';
 import * as loginReducer from './loginReducer';
 import * as themeReducer from './themeReducer';
-export default Object.assign(loginReducer, loadingReducer, themeReducer);
+// import * as swipeReducer from './swipeReducer';
+// import * as matchReducer from './matchReducer';
+// import * as streamReducer from './streamReducer';
+// import * as feedReducer from './feedReducer';
+export default Object.assign(loginReducer, loadingReducer, themeReducer, swipeReducer, matchReducer, feedReducer,);
